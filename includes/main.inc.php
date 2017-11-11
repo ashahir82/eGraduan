f1f2e9351c98be7906dbbda2445d8e8f5d2c6859
ashahir82 <ashahir@gmail.com>
2017-11-12 01:45:55 +0800

    modified login page

--- includes/main.inc.php
+++ includes/main.inc.php
@@ -1,5 +1,75 @@
 <div class="section content">
 	<div class="container">
-		test
+		<div class="panel panel-success panel-search">
+			<div class="panel-heading text-center">
+				<h2 class="panel-title">SEMAKAN GRADUAN</h2>
+			</div>
+			<div class="panel-body">
+				<form class="form-horizontal form-search">
+					<div class="form-group">
+						<label for="inputEmail3" class="col-sm-4 control-label">Daftar Pelajar</label>
+						<div class="col-sm-7">
+							<input type="email" class="form-control" id="inputEmail3" placeholder="Nombor Daftar Pelajar">
+						</div>
+						<span id="helpBlock" class="col-sm-1 help-block">atau<span>
+					</div>
+					<div class="form-group">
+						<label for="inputPassword3" class="col-sm-4 control-label">Kad Pengenalan</label>
+						<div class="col-sm-7">
+							<input type="password" class="form-control" id="inputPassword3" placeholder="Nombor Kad Pengenalan">
+						</div>
+						<span id="helpBlock" class="col-sm-1 help-block">atau<span>
+					</div>
+					<div class="form-group">
+						<label for="inputPassword3" class="col-sm-4 control-label">Nama</label>
+						<div class="col-sm-7">
+							<input type="password" class="form-control" id="inputPassword3" placeholder="Nama Pelajar">
+						</div>
+						<span id="helpBlock" class="col-sm-1 help-block"><span>
+					</div>
+					<div class="form-group">
+						<div class="col-sm-offset-4 col-sm-8">
+							<button type="submit" class="btn btn-success">Semak</button>
+							<button type="submit" class="btn btn-warning">Batal</button>
+						</div>
+					</div>
+				</form>
+			</div>
+			<div class="panel-footer">
+				<p class="text-justify">Maklumat ini tepat untuk pelajar keluaran dari sesi <strong>2/2011</strong> dan seterusnya. Untuk maklumat lanjut sila hubungi <strong>Bahagian Kawalan Kualiti Latihan ILPKL</strong>.</p>
+			</div>
+		</div>
+		
+		<div class="panel panel-primary">
+			<div class="panel-heading text-center">
+				<h2 class="panel-title">KEPUTUSAN SEMAKAN</h2>
+			</div>
+			<div class="panel-body">
+				<p>...</p>
+			</div>
+
+			<!-- Table -->
+			<div class="table-responsive">
+				<table class="table table-hover table-condensed">
+					<tr class="info">
+						<th>#</th>
+						<th>Nama</th>
+						<th>Sesi Keluaran</th>
+						<th>Konvokesyen</th>
+						<th>Tindakan</th>
+					</tr>
+					<tr>
+						<td>...</td>
+						<td>...</td>
+						<td>...</td>
+						<td>...</td>
+						<td>...</td>
+					</tr>
+					<tr>
+						<td colspan="5" class="danger">Tiada maklumat ditemui.</td>
+					</tr>
+				</table>
+			</div>
+		</div>
 	</div>
 </div>
