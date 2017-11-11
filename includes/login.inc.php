65dfa00a70ac66ccd7e1da321c843a3500b55d8c
ashahir82 <ashahir@gmail.com>
2017-11-12 00:05:05 +0800

    add login page

---
+++ includes/login.inc.php
@@ -0,0 +1,16 @@
+<?php
+logged_in_redirect();
+?>
+<div class="section">
+	<div class="container">
+		<form class="form-signin">
+			<h2 class="form-signin-heading">Sila Daftar Masuk</h2>
+			<label for="inputEmail" class="sr-only">Nama Pengguna</label>
+			<input type="email" id="inputEmail" class="form-control" placeholder="Nama Pengguna" required autofocus>
+			<label for="inputPassword" class="sr-only">Kata Laluan</label>
+			<input type="password" id="inputPassword" class="form-control" placeholder="Kata Laluan" required>
+			<div class="checkbox">
+			<button class="btn btn-lg btn-primary btn-block" type="submit">Daftar Masuk</button>
+		</form>
+	</div>
+</div>
